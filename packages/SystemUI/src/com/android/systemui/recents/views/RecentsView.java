0c23ad2e178baad9aff5d109e1d5b0ee64cbc1dd
Clark Scheff <clark@cyngn.com>
2014-11-14 09:44:51 -0800

    Add clear all recents action to recents panel
    Change-Id: I2243ebc83abc483e78b112900a653d7e61edc5ba
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
    	packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
    	packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -40,6 +40,8 @@ import com.android.systemui.recents.model.RecentsTaskLoader;
 import com.android.systemui.recents.model.Task;
 import com.android.systemui.recents.model.TaskStack;
 
+import com.android.systemui.R;
+
 import java.util.ArrayList;
 
 /**
@@ -66,6 +68,7 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
     View mSearchBar;
     RecentsViewCallbacks mCb;
     View mClearRecents;
+    boolean mAlreadyLaunchingTask;
 
     public RecentsView(Context context) {
         super(context);
@@ -158,6 +161,17 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
             if (child != mSearchBar) {
                 TaskStackView stackView = (TaskStackView) child;
                 //stackView.dismissAllTasks();
+            }
+        }
+    }
+
+    public void dismissAllTasksAnimated() {
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            if (child != mSearchBar) {
+                TaskStackView stackView = (TaskStackView) child;
+                stackView.dismissAllTasks();
             }
         }
     }
@@ -326,12 +340,12 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
     @Override
     protected void onAttachedToWindow () {
         super.onAttachedToWindow();
-        //mClearRecents = ((View)getParent()).findViewById(R.id.clear_recents);
-        //mClearRecents.setOnClickListener(new View.OnClickListener() {
-        //    public void onClick(View v) {
-        //        dismissAllTasksAnimated();
-        ///    }
-       // });
+        mClearRecents = ((View)getParent()).findViewById(R.id.clear_recents);
+        mClearRecents.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dismissAllTasksAnimated();
+            }
+        });
     }
 
     /**
