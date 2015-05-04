Jackeagle/caf_frameworks_base

SystemUI: Fix data tile doesn't change with default data card issue
By current design, the data tile can control all cards data, and it
refresh its state according to the first card.
Dds tile also refresh its state according to the first card.
When data on/off, the changed URI is Settings.Global.MOBILE_DATA + 0/1,
but not Settings.Global.MOBILE_DATA.

The data tile should just control the default data card and refresh
according to it.
Change the URI to watch for data changes and dds switch.

CRs-Fixed: 755171

Change-Id: I73ab6b40f4ef28fccb3046b6dbc63b754356ea91

packages/SystemUI/src/com/android/systemui/qs/tiles/DataTile.java
@@ -88,11 +88,13 @@ public class DataTile extends QSTile<QSTile.BooleanState> {
             Settings.Global.putInt(mContext.getContentResolver(),
                     Settings.Global.MOBILE_DATA + i, (enabled) ? 1 : 0);
         }
+        mTelephonyManager.setDataEnabledUsingSubId(
+                SubscriptionManager.getDefaultDataSubId(), enabled);
     }
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        final boolean dataOn = mTelephonyManager.getDataEnabled()
+        final boolean dataOn = isDefaultDataEnabled()
                 && !isAirplaneModeOn() && hasIccCard();
         state.value = dataOn;
         state.visible = true;
@@ -106,6 +108,11 @@ public class DataTile extends QSTile<QSTile.BooleanState> {
             state.contentDescription = mContext.getString(
                     R.string.accessibility_quick_settings_data_off);
         }
+    }
+
+    private boolean isDefaultDataEnabled() {
+        return Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.MOBILE_DATA + SubscriptionManager.getDefaultDataPhoneId(), 0) != 0;
     }
 
     public void setListening(boolean listening) {
@@ -131,9 +138,18 @@ public class DataTile extends QSTile<QSTile.BooleanState> {
         }
 
         public void startObserving() {
-            mContext.getContentResolver().registerContentObserver(
-                    Settings.Global.getUriFor(Settings.Global.MOBILE_DATA),
-                    false, this);
+            int phoneCount = mTelephonyManager.getPhoneCount();
+            for (int i = 0; i < phoneCount; i++) {
+                mContext.getContentResolver().registerContentObserver(
+                        Settings.Global.getUriFor(Settings.Global.MOBILE_DATA + i),
+                        false, this);
+            }
+            if (mTelephonyManager.isMultiSimEnabled()) {
+                mContext.getContentResolver().registerContentObserver(
+                        Settings.Global.getUriFor(
+                        Settings.Global.MULTI_SIM_DATA_CALL_SUBSCRIPTION),
+                        false, this);
+            }
             mContext.getContentResolver().registerContentObserver(
                     Settings.Global.getUriFor(Settings.Global.AIRPLANE_MODE_ON),
                     false, this);

mitwo-dev/platform_frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/qs/tiles/DataTile.java

packages/SystemUI/src/com/android/systemui/qs/tiles/DataTile.java
@@ -80,18 +80,13 @@ public class DataTile extends QSTile<QSTile.BooleanState> {
                 Settings.Global.AIRPLANE_MODE_ON, 0) != 0 || !hasIccCard()) {
             return;
         }
-        int phoneCount = mTelephonyManager.getPhoneCount();
-        for (int i = 0; i < phoneCount; i++) {
-            Settings.Global.putInt(mContext.getContentResolver(),
-                    Settings.Global.MOBILE_DATA + i, (enabled) ? 1 : 0);
-            int[] subId = SubscriptionManager.getSubId(i);
-            mTelephonyManager.setDataEnabled(subId[0], enabled);
-        }
+        mTelephonyManager.setDataEnabled(
+                SubscriptionManager.from(mContext).getDefaultDataSubId(), enabled);
     }
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        final boolean dataOn = mTelephonyManager.getDataEnabled()
+        final boolean dataOn = isDefaultDataEnabled()
                 && !isAirplaneModeOn() && hasIccCard();
         state.value = dataOn;
         state.visible = true;
@@ -105,6 +100,12 @@ public class DataTile extends QSTile<QSTile.BooleanState> {
             state.contentDescription = mContext.getString(
                     R.string.accessibility_quick_settings_data_off);
         }
+    }
+
+    private boolean isDefaultDataEnabled() {
+        return Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.MOBILE_DATA +
+                SubscriptionManager.from(mContext).getDefaultDataPhoneId(), 0) != 0;
     }
 
     public void setListening(boolean listening) {
@@ -130,9 +131,18 @@ public class DataTile extends QSTile<QSTile.BooleanState> {
         }
 
         public void startObserving() {
-            mContext.getContentResolver().registerContentObserver(
-                    Settings.Global.getUriFor(Settings.Global.MOBILE_DATA),
-                    false, this);
+            int phoneCount = mTelephonyManager.getPhoneCount();
+            for (int i = 0; i < phoneCount; i++) {
+                mContext.getContentResolver().registerContentObserver(
+                        Settings.Global.getUriFor(Settings.Global.MOBILE_DATA + i),
+                        false, this);
+            }
+            if (mTelephonyManager.isMultiSimEnabled()) {
+                mContext.getContentResolver().registerContentObserver(
+                        Settings.Global.getUriFor(
+                        Settings.Global.MULTI_SIM_DATA_CALL_SUBSCRIPTION),
+                        false, this);
+            }
             mContext.getContentResolver().registerContentObserver(
                     Settings.Global.getUriFor(Settings.Global.AIRPLANE_MODE_ON),
                     false, this);